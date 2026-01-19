c51e73b0a17b1128f6624aed7793a21c6a145baf
vpagidi6 <vamshipagidi6@gmail.com>
2026-01-19 17:32:37 -0600

    demo functionality created

---
+++ demo/mock_photo_send.py
@@ -0,0 +1,24 @@
+import requests
+
+def send_image(img):
+    url = "http://50.19.22.145:8000/classify-image"
+
+    image_file = open(img, "rb")
+    data = {
+        'image': image_file
+    }
+
+    response = requests.post(
+        url,
+        files=data,
+        timeout=10
+    )
+
+    if response.status_code == 200:
+        print(f'Successfully sent {img}')
+    else:
+        print('Error in POST request')
+
+    image_file.close()
+
+send_image("/Users/vamshipagidi/Development/Safedrive/demo/driver_images/driver_image(0).png")
